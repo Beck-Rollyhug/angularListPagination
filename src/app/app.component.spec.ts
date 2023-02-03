7a03f34e47941b5020a2e0205a6056bf6519d549
Aleksey <my.beck.for.work@mail.ru>
2023-02-03 12:49:48 +0500

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularLlistPagination'`, () => {
+  it(`should have as title 'angularListPagination'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularLlistPagination');
+    expect(app.title).toEqual('angularListPagination');
   });
 
   it('should render title', () => {
